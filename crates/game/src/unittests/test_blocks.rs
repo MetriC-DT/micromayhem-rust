c561514af5be14b80d2434fb1a08e3409575cd74
Derick Tseng <derickboss1@gmail.com>
2021-12-30 21:35:07 -0800

    fix test

--- crates/game/src/unittests/test_blocks.rs
+++ crates/game/src/unittests/test_blocks.rs
@@ -2,6 +2,6 @@ use crate::block::{get_block_friction, BlockType};
 
 #[test]
 fn test_get_block_friction() {
-    assert_eq!(get_block_friction(BlockType::GrassBlock), 1.0);
-    assert_eq!(get_block_friction(BlockType::IceBlock), 0.5);
+    assert_eq!(get_block_friction(BlockType::GrassBlock), 0.8);
+    assert_eq!(get_block_friction(BlockType::IceBlock), 0.2);
 }
